954abba665083a41ed3ae44bb806208ba32d0d3b
Jose Andres Cartuche (Aceleración) <joseandres.cv@hotmail.com>
2018-04-19 17:55:30 -0500

    file index.php
    creando un index.php de prueba

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es_ES">
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Prueba PHP">
+	<title>Prueba PHP</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="../style.css">
+</head>
+<body>
+	<img src="https://borealacademy.files.wordpress.com/2014/09/php.png" alt="">
+	<?php
+		/* Comentario inicial */
+		echo '<h2>El módulo PHP funciona correctamente....</h2>';
+		// Comentario tipo C, C++
+		# Comentario final tipo shell de unix y python
+	?>
+</body>
+</html>
